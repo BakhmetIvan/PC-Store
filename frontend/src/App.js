ea4cb19b0ca010aec6921fdbd56a56a2639e3057
Serhii Makarenko <s.makarenko2003@gmail.com>
2024-05-09 21:58:02 +0300

    added frontend uploaded

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import Login from './login/login';
+import Register from './register/register';
+import Header from './header/header';
+import Filter from './filter/filter';
+import Product from './product/product';
+import Footer from './footer/footer';
+import Promotion from './promotion/promotion';
+import ProductsCategory from './productsCategory/productsCategory';
+import ProfileMenu from './profileMenu/profileMenu';
+import MyOrders from './myOrders/myOrders';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="App">
+      <h1>hola amigos</h1>
+      <Login/>
+      <h1>hola amigos</h1>
+      <Register/>
+      </div>
+      <h1>hola amigos</h1>
+      <Filter/>
+      <h1>hola amigos</h1>
+      <div className="App">
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      </div>
+      <Footer/>
+      <h1>Hola amigos</h1>
+      <Promotion/>
+      <ProductsCategory title="Ноутбуки"/>
+      <ProductsCategory title="Монітори"/>
+      <ProductsCategory title="Відеокарти"/>
+      <div className="OOOOrders">
+        <ProfileMenu/>
+        <MyOrders/>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ frontend/src/footer/footer.js
@@ -0,0 +1,56 @@
+import { Component } from "react";
+import './footer.css';
+import instaIcon from '../img/instagram 1.svg';
+import clockIcon from '../img/clock-regular 1.svg';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div className="wrapper">
+                <footer className="footer">
+                    <div className="footer__content">
+                        <div className="footer__logo logo">Logo</div>
+                        <div className="footer__instas-list">
+                            <div className="footer__title">Соцмережі</div>
+                            <ul className="footer__instas">
+                                <li className="footer__insta">
+                                    <a href="#" className="footer_insta-link">
+                                        <div className="footer__insta-logo"><img src={instaIcon} alt="" /></div>
+                                        <div className="footer__insta-name">Чернявский Павло</div>
+                                    </a>
+                                </li>
+                                <li className="footer__insta">
+                                    <a href="#" className="footer_insta-link">
+                                        <div className="footer__insta-logo"><img src={instaIcon} alt="" /></div>
+                                        <div className="footer__insta-name">Бахмет Іван</div>
+                                    </a>
+                                </li>
+                                <li className="footer__insta">
+                                    <a href="#" className="footer_insta-link">
+                                        <div className="footer__insta-logo"><img src={instaIcon} alt="" /></div>
+                                        <div className="footer__insta-name">Макаренко Сергій</div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="footer__contact">
+                            <div className="footer__title">Контактний номер</div>
+                            <ul className="footer__contact-list">
+                                <li className="footer__contact-vodafone">+38 099 000 99 99</li>
+                            </ul>
+                        </div>
+                        <div className="footer__timetable">
+                            <div className="footer__title">Графік роботи</div>
+                            <div className="footer__timetable-item">
+                                <img src={clockIcon} alt="" />
+                                <span>ПН - НД 9:00 - 21:00</span>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Footer;
